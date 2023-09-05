04925540608f1c5547ef5edede588b4c3a9a2303
Serhii Vostrikov <65418387+Antiserggio@users.noreply.github.com>
2023-09-05 23:14:51 +0300

    05

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsEl = ingredients.map((ingredient) => {
+  console.log(ingredient);
+  const itemEL = document.createElement("li");
+  itemEL.textContent = ingredient;
+  itemEL.classList.add("item");
+
+  return itemEL;
+});
+console.log(ingredientsEl);
+ingredientsList.append(...ingredientsEl);
+console.log(ingredientsList);
---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#name-input");
+
+const output = document.querySelector("#name-output");
+
+input.addEventListener("input", handleInput);
+
+function handleInput(event) {
+  output.textContent = event.currentTarget.value;
+  if (event.currentTarget.value !== "") {
+    output.textContent === event.currentTarget.value;
+  } else {
+    output.textContent = "Anonimus";
+  }
+}
