bee2b75406ff417e8d3872f2a6d87bbfa8010e76
Serhii Vostrikov <65418387+Antiserggio@users.noreply.github.com>
2023-09-06 01:22:06 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,17 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", handleBlur);
+
+function handleBlur() {
+  const inputDataSetLength = Number(input.getAttribute("data-length"));
+
+  const inputValueLength = Number(input.value.trim().length);
+
+  if (inputDataSetLength === inputValueLength) {
+    input.classList.add("valid");
+    input.classList.remove("invalid");
+  } else {
+    input.classList.add("invalid");
+    input.classList.remove("valid");
+  }
+}
